d7b47355ccb950896e9e23d5706cbac696a20380
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-24 08:12:31 +0000

    bellman-ford

--- minimumPath/bellman-ford/bellman-ford.cpp
+++ minimumPath/bellman-ford/bellman-ford.cpp
@@ -1,7 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+struct Graph{
+    int nV, nE;
+    vector<vector<int>> edges;
+};
+void testCase() {
+    int nV, nE, start;
+    cin >> nV >> nE >> start;
+    Graph *g = new Graph;
+    g->nV = nV;
+    g->nE = nE;
+    g->edges.resize(nV);
+    
+}
 int main() {
     // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
     return 0;
 }
