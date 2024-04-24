dd5d487a6238ae70f2b6e2cf334a3c2f8b9f5769
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-24 08:55:43 +0000

    add bellman-ford

--- minimumPath/bellman-ford/bellman-ford.cpp
+++ minimumPath/bellman-ford/bellman-ford.cpp
@@ -1,20 +1,48 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-struct Graph{
+const int MAX = 1e7;
+struct Graph {
     int nV, nE;
     vector<vector<int>> edges;
 };
+void bellman_ford(Graph* g, int start) {
+    vector<int> dist(g->nV, MAX);
+    dist[start] = 0;
+    for (int k = 0; k < g->nV - 1; k++) {
+        for (int j = 0; j < g->nV; j++) {
+            for (int i = 0; i < g->nV; i++) {
+                if (g->edges[j][i] != MAX && dist[i] > dist[j] + g->edges[j][i]) {
+                    dist[i] = dist[j] + g->edges[j][i];
+                }
+            }
+        }
+    }
+    cout << "Shortest path from vertex " << start + 1 << " to other vertices: ";
+    for (int i = 0; i < g->nV; i++) {
+        if (dist[i] == MAX) {
+            cout << "INF ";
+        } else {
+            cout << dist[i] << " ";
+        }
+        
+    }
+}
 void testCase() {
     int nV, nE, start;
     cin >> nV >> nE >> start;
-    Graph *g = new Graph;
+    Graph* g = new Graph;
     g->nV = nV;
     g->nE = nE;
-    g->edges.resize(nV);
-    
+    g->edges.resize(nV, vector<int>(nV, MAX));
+    for (int i = 0; i < nE; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        g->edges[u - 1][v - 1] = w;
+    }
+    bellman_ford(g, start - 1);
 }
 int main() {
-    // Write your code here
     int t;
     cin >> t;
     while (t--) {
